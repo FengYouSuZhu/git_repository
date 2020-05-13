ecd87496a8983df04afbdb9e0199f826e2eed4dc
FengYouSuZhu <702684067@qq.com>
2020-05-13 19:29:38 +0800

    创建

---
+++ GitDemo/src/main/java/com/asus/java/Test1.java
@@ -0,0 +1,9 @@
+package com.asus.java;
+
+/**
+ * @author 枫柚素主
+ * @version 1.0
+ * @date 2020/5/13 19:27
+ */
+public class Test1 {
+}
---
+++ GitDemo/src/test/java/com/asus/test/Test2.java
@@ -0,0 +1,9 @@
+package com.asus.test;
+
+/**
+ * @author 枫柚素主
+ * @version 1.0
+ * @date 2020/5/13 19:28
+ */
+public class Test2 {
+}
